479f49c1df6b5e4957e6fb4a52c1604e241e78c0
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2019-12-08 13:35:57 +0100

    dynamic api  url created

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const inPutField = document.querySelector("#inputValue");
 const searchButton = document.querySelector("#search");
 const API_KEY = 'a7026b99934afbac930f68b59ae6c22f';
+const url = 'https://api.themoviedb.org/3/search/movie?api_key=a7026b99934afbac930f68b59ae6c22f';
 
 // searchButton.addEventListener('click', function (event) {
 //     event.preventDefault();
@@ -10,5 +11,22 @@ const API_KEY = 'a7026b99934afbac930f68b59ae6c22f';
 
 searchButton.onclick = function (event) {
     event.preventDefault();
-    console.log(inPutField.value);
+    const value = inPutField.value;
+
+
+    const dynamicUrl = url + '&query=' + value;
+
+
+    fetch(dynamicUrl)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log('Data: ', data);
+        })
+        .catch((error) => {
+            console.log('Error: ', error);
+
+        });
+
+    console.log('Value: ', value);
+
 };
