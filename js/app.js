a40ed68d9f991311005583593a2cf57694152ee8
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2019-12-08 13:12:50 +0100

    adding api key

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 const inPutField = document.querySelector("#inputValue");
 const searchButton = document.querySelector("#search");
+const API_KEY = 'a7026b99934afbac930f68b59ae6c22f';
 
 // searchButton.addEventListener('click', function (event) {
 //     event.preventDefault();
 //     console.log("Clicked");
 
 // })
+
+searchButton.onclick = function (event) {
+    event.preventDefault();
+    console.log(inPutField.value);
+};
