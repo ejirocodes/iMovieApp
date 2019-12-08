c20750aebe6a1448e305764abd339402d8399802
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2019-12-08 12:25:27 +0100

    input field and button selected

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const inPutField = document.querySelector("#inputValue");
+const searchButton = document.querySelector("#search");
+
+// searchButton.addEventListener('click', function (event) {
+//     event.preventDefault();
+//     console.log("Clicked");
+
+// })
