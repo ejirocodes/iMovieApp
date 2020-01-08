020a5c29db99e84b4ea9b296d664cfbe0f7b169b
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-01-08 13:45:30 +0100

    activate service worker

--- sw.js
+++ sw.js
@@ -17,7 +17,12 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(cacheName)
             .then(cache => {
-                cache.addAll(filesToCache);
+               return cache.addAll(filesToCache);
             })
     );
+});
+
+self.addEventListener('activate', event => {
+    console.log('activating service worker...');
+    return self.clients.claim();
 });
