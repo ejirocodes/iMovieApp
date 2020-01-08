5439e5f6443b630b57b0a06eadc73144eb799f40
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-01-08 13:17:26 +0100

    install service worker && cache static files

--- sw.js
+++ sw.js
@@ -1,3 +1,23 @@
+const filesToCache = [
+    '/',
+    'index.html',
+    'assets/css/style.css',
+    'assets/js/app.js',
+    'assets/js/apiTransaction.js',
+    'assets/favicon/android-chrome-192x192.png',
+    'assets/favicon/android-chrome-512x512.png',
+    'assets/favicon/favicon-32x32.png',
+    'manifest.json'
+];
+
+const cacheName = 'imovie-app-v1';
+
 self.addEventListener('install', event => {
-    
-})
+    console.log('installing service worker and caching static assets...😎😎');
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                cache.addAll(filesToCache);
+            })
+    );
+});
