06c76e05eb00ce04469dcfe110ebc010ff08cdb9
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2020-01-08 14:21:25 +0100

    intercept network request, add custom offline and 404 page

--- sw.js
+++ sw.js
@@ -29,3 +29,36 @@ self.addEventListener('activate', event => {
     console.log('activating service worker...');
     return self.clients.claim();
 });
+
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    console.log('Found ', event.request.url, ' in cache');
+                    return response;
+                }
+                console.log('Network request for ', event.request.url);
+                return fetch(event.request)
+                    .then(response => {
+                        // Respond with custom 404 page
+                        if (response.status === 404) {
+                            return caches.match('./pages/404.html');
+                        }
+
+                        return caches.open(staticCacheName)
+                            .then(cache => {
+                                cache.put(event.request.url, response.clone());
+                                return response;
+                            });
+                    });
+
+            }).catch(error => {
+                console.log('You have a ', error);
+                //  Respond with custom offline page
+                return caches.match('./pages/offline.html');
+            })
+    );
+});
